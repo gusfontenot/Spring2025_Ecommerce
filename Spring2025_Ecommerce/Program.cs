f34b5ff1fe6eab80938dabf583ffea15ae48ef32
gusfo <gusfo@GUSCOMPUTER>
2025-02-06 13:24:42 -0500

    Add project files.

---
+++ Spring2025_Ecommerce/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Spring2025_Ecommerce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
