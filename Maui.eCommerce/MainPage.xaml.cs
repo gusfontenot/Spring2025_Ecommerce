1a31942e536661910c4e2254f85ea9e88f8d500e
Gus Fontenot <gusfontenotcollege@gmail.com>
2025-02-16 13:51:08 -0500

    Added inventory management view and added navigation.

--- Maui.eCommerce/MainPage.xaml.cs
+++ Maui.eCommerce/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Maui.eCommerce
+﻿using Maui.eCommerce.ViewModels;
+
+namespace Maui.eCommerce
 {
     public partial class MainPage : ContentPage
     {
@@ -7,18 +9,12 @@
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainViewModel();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void InventoryClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            Shell.Current.GoToAsync("//InventoryManagement");
         }
     }
 
