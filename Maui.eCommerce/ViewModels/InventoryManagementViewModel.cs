bf4a6900b189b75380550c0f2175e4789864f529
Gus Fontenot <gusfontenotcollege@gmail.com>
2025-02-18 16:23:45 -0500

    Added UI for InventoryManagementView with the delete buttons.

---
+++ Maui.eCommerce/ViewModels/InventoryManagementViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Library.eCommerce.Services;
+using Spring2025_Ecommerce.Models;
+
+namespace Maui.eCommerce.ViewModels
+{
+    public class InventoryManagementViewModel
+    {
+        public Product? SelectedProduct { get; set; }
+        private ProductServiceProxy _svc = ProductServiceProxy.Current;
+        public List<Product?> Products
+        {
+            get
+            {
+                return _svc.Products;
+            }
+        }
+
+        public Product? Delete()
+        {
+            return _svc.Delete(SelectedProduct?.Id ?? 0);
+        }
+    }
+}
--- Maui.eCommerce/Views/InventoryManagementView.xaml.cs
+++ Maui.eCommerce/Views/InventoryManagementView.xaml.cs
@@ -1,3 +1,6 @@
+using Library.eCommerce.Services;
+using Maui.eCommerce.ViewModels;
+
 namespace Maui.eCommerce.Views;
 
 public partial class InventoryManagementView : ContentPage
@@ -5,10 +8,16 @@ public partial class InventoryManagementView : ContentPage
 	public InventoryManagementView()
 	{
 		InitializeComponent();
+		BindingContext = new InventoryManagementViewModel();
 	}
 
     private void CancelClicked(object sender, EventArgs e)
     {
 		Shell.Current.GoToAsync("//MainPage");
+    }
+
+    private void DeleteClicked(object sender, EventArgs e)
+    {
+        (BindingContext as InventoryManagementViewModel)?.Delete();
     }
 }
